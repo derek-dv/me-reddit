23b07e3267cabc57e7cc9fb2c0a8b5693c7c9dfa
Derek <derekmiracledavid@hotmail.com>
2021-08-07 17:06:21 +0100

    HomeSearch completed

--- react/src/components/HomeSearch.tsx
+++ react/src/components/HomeSearch.tsx
@@ -2,23 +2,21 @@ import "../styles/HomeSearch.css";
 
 const HomeSearch = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__left">
-        <div className="navbar__logo">LOGO</div>
-        <div className="navbar__links">
-          <a href="/">Home</a>
-          <a href="/">About</a>
+    <form className="search">
+      <input placeholder="search" type="text" className="search__input" />
+      <div className="search__text">Filter by</div>
+      <div className="search__checks">
+        <div className="search__check">
+          <input type="checkbox" id="author" />
+          <label htmlFor="author">Author</label>
+        </div>
+        <div className="search__check">
+          <input type="checkbox" id="title"/>
+          <label htmlFor="title">Title</label>
         </div>
       </div>
-      <div className="navbar__right">
-        <a href="/" className="navbar__auths">
-          Login
-        </a>
-        <a href="/" className="navbar__auths">
-          Register
-        </a>
-      </div>
-    </div>
+      <button className="search__button" type="submit">Search</button>
+    </form>
   );
 };
 
