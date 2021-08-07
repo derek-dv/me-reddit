af8837b45e4829b27a172cc8e936b2c201b4dd30
Derek <derekmiracledavid@hotmail.com>
2021-08-07 12:38:19 +0100

    --

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,24 +1,11 @@
-import Navbar from "./components/Navbar"
-import './App.css';
+import "./App.css";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">
-    <Navbar /> 
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div className="App"></div>
+    </Layout>
   );
 }
 
