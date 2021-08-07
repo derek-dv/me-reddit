7d621b5c516a85349904757366da349d1abdf992
Derek <derekmiracledavid@hotmail.com>
2021-08-07 09:23:13 +0100

    divided the folder into backend and frontend

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Navbar /> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ react/src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar: React<any> = () => (
+    <div>Hello</div>
+)
+
+export default Navbar
