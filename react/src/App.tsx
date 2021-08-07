5b2e79b0baafe97c023b94e209558a25a1a6f0b1
Derek <derekmiracledavid@hotmail.com>
2021-08-07 16:28:42 +0100

    Revert "search component created and added to home page"
    This reverts commit f348655187fd4ba921a89d18ace838645f1719f2.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import HomeSearch from "./components/HomeSearch";
 import Layout from "./components/layout";
 
 function App() {
   return (
     <Layout>
-      <div className="app">
-        <HomeSearch />
-      </div>
+      <div className="App"></div>
     </Layout>
   );
 }
