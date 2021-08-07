c1dac686976b664020fe45d0c0eda4b6bc74af72
Derek <derekmiracledavid@hotmail.com>
2021-08-07 13:24:18 +0100

    Completed styling navbar

--- react/src/components/Navbar.tsx
+++ react/src/components/Navbar.tsx
@@ -1,3 +1,25 @@
-const Navbar: React.FC<any> = () => <div className="py-3">Hello</div>;
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__left">
+        <div className="navbar__logo">LOGO</div>
+        <div className="navbar__links">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+        </div>
+      </div>
+      <div className="navbar__right">
+        <a href="/" className="navbar__auths">
+          Login
+        </a>
+        <a href="/" className="navbar__auths">
+          Register
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
