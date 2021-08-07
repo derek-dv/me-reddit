0331052ed98223616dd591353a92b0c5880ea800
Derek <derekmiracledavid@hotmail.com>
2021-08-07 16:20:51 +0100

    error

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+// import HomePosts from "./components/HomePosts";
 import HomeSearch from "./components/HomeSearch";
 import Layout from "./components/layout";
 
@@ -7,9 +8,10 @@ function App() {
     <Layout>
       <div className="app">
         <HomeSearch />
+        {/* <HomePosts/> */}
       </div>
     </Layout>
-  );
+  ); 
 }
 
 export default App;
