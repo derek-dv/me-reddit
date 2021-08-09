c973bfd3476e7f2a0544550c6323a082031a4f2a
Derek <derekmiracledavid@hotmail.com>
2021-08-09 20:48:43 +0100

    register page functionalities started

--- react/src/pages/Register.tsx
+++ react/src/pages/Register.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/auth.css";
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
   return (
     <div className="auth">
       <div className="auth__form">
@@ -14,18 +18,35 @@ const Register: React.FC<RegisterProps> = ({}) => {
           <form>
             <div className="auth__field">
               <label htmlFor="email">Email</label>
-              <input type="text" id="email" placeholder="Enter email" />
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                placeholder="Enter email"
+                required
+              />
             </div>
             <div className="auth__field">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" placeholder="Enter username" />
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e)=>setUsername(e.target.value)}
+                placeholder="Enter username"
+                required
+              />
             </div>
             <div className="auth__field">
               <label htmlFor="password1">Password</label>
               <input
                 type="password"
                 id="password1"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
                 placeholder="Enter password"
+                required
               />
             </div>
             <div className="auth__field">
@@ -33,9 +54,22 @@ const Register: React.FC<RegisterProps> = ({}) => {
               <input
                 type="password"
                 id="password2"
+                value={password2}
+                onChange={(e)=>setPassword2(e.target.value)}
                 placeholder="Confirm password"
+                required
               />
             </div>
+            <button
+              className="auth__button"
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                console.log(e.target);
+              }}
+            >
+              Register
+            </button>
           </form>
         </div>
       </div>
